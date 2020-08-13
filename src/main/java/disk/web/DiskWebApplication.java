304bfb163a910a0eadfe62f9097e1ba206d7d56b
guangming_li <guangming_li@sui.com>
2020-08-13 13:47:52 +0800

    前端框架搭建

---
+++ src/main/java/disk/web/DiskWebApplication.java
@@ -0,0 +1,13 @@
+package disk.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiskWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DiskWebApplication.class, args);
+    }
+
+}
---
+++ src/test/java/disk/web/DiskWebApplicationTests.java
@@ -0,0 +1,13 @@
+package disk.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiskWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
